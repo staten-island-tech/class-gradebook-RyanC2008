a50d4c4891399bd6e54acb17b6b15c3f17a064d6
RyanC2008 <113192723+RyanC2008@users.noreply.github.com>
2022-10-06 10:31:40 -0400

    Create classgradebook2.py

---
+++ classgradebook2.py
@@ -0,0 +1,16 @@
+student_list = []
+grade_list = []
+
+def find_average(grade_list):
+    return sum(grade_list) / len(grade_list)
+
+while True:
+    student_name = input('Please input student name here: ')
+    print('Thank you!')
+    print(f'Please enter grades for {student_name} here: ')
+    break
+
+
+
+    
+    
